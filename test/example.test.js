c97fde997cd0dd854d9edafd275bc5ab8a796fbb
armaBurton <armanarma@armaburton.com>
2021-12-17 11:30:12 -0800

    arma mob push

--- test/example.test.js
+++ test/example.test.js
@@ -4,16 +4,24 @@ import { renderPoll } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('renderPoll function should create 5 p tags and div and append all p to div', (expect) => {
+const renderObj = {
+    question: `What's your favorite color`,
+    option_a: `blue`,
+    option_b: `red`,
+    votes_a: `9000`,
+    votes_b: `2`
+};
+
+test('renderPoll function should create 5 p tags and div and append all p to div ', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="poll-item"><p>What's your favorite color</p><p>blue</p><p>9000</p><p>red</p><p>2</p></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderPoll(renderObj);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
